854763f57bfcdb9175218701569d2da1093046d3
Gustavo Cirino2802 <gustavoalmeida.cirino@gmail.com>
2023-10-27 11:23:23 -0300

    Sites

---
+++ Introdução Javascript/main.js
@@ -0,0 +1,7 @@
+console.log('Arquivo main.js foi carregado com sucesso!');
+
+//var btnTerceiro = document.getElementsByClassName('btnTerceiro')[0];
+var btnTerceiro = document.querySelector('.btnTerceiro');
+console.log('btnTerceiro');
+
+btnTerceiro.addEventListener('click',);
